corrected eer computation

wave_u_net_architecture/test.py
@@ -142,6 +142,8 @@ def validate(args, model, criterion, test_data):
     total_eer = 0.
     total_accuracy = 0.
     with tqdm(total=len(test_data) // args.batch_size) as pbar, torch.no_grad():
+        all_predictions = []
+        all_targets = []
         for example_num, (x, targets) in enumerate(dataloader):
             if args.cuda:
                 x = x.cuda()
@@ -149,7 +151,9 @@ def validate(args, model, criterion, test_data):
                 # for k in list(targets.keys()):
                 #     targets[k] = targets[k].cuda()
 
-            _, avg_loss, avg_eer, avg_accuracy = compute_loss(model, x, targets, criterion)
+            outputs, avg_loss, avg_eer, avg_accuracy = compute_loss(model, x, targets, criterion)
+            all_predictions.extend([o.item() for o in outputs['all']])
+            all_targets.extend([t.item() for t in targets])
 
             total_loss += (1. / float(example_num + 1)) * (avg_loss - total_loss)
             total_eer += (1. / float(example_num + 1)) * (avg_eer - total_eer)
@@ -157,5 +161,7 @@ def validate(args, model, criterion, test_data):
 
             pbar.set_description("Current loss: {}, eer: {}, accuracy: {}".format(total_loss, total_eer, total_accuracy))
             pbar.update(1)
+
+        total_eer = utils.compute_eer_total(all_predictions, all_targets)
 
     return total_loss, total_eer, total_accuracy

wave_u_net_architecture/train.py
@@ -163,6 +163,8 @@ while state["worse_epochs"] < args.patience:
     with tqdm(total=len(train_data) // args.batch_size) as pbar:
         np.random.seed()
         update_count = 0
+        all_predictions = []
+        all_targets = []
         for example_num, (x, targets) in enumerate(dataloader):
             if args.cuda:
                 x = x.cuda()
@@ -179,6 +181,8 @@ while state["worse_epochs"] < args.patience:
             # Compute loss for each instrument/model
             optimizer.zero_grad()
             outputs, avg_loss, avg_eer, avg_accuracy = utils.compute_loss(model, x, targets, criterion, compute_grad=True)
+            all_predictions.extend([o.item() for o in outputs['all']])
+            all_targets.extend([t.item() for t in targets])
             optimizer.step()
             update_count += 1
 
@@ -189,7 +193,6 @@ while state["worse_epochs"] < args.patience:
             avg_time += (1. / float(example_num + 1)) * (t - avg_time)
 
             writer.add_scalar("train_loss", avg_loss, state["step"])
-            writer.add_scalar('train_eer', avg_eer, state['step'])
             writer.add_scalar('train_accuracy', avg_accuracy, state['step'])
 
             # if example_num % args.example_freq == 0:
@@ -203,6 +206,9 @@ while state["worse_epochs"] < args.patience:
             pbar.update(1)
             # if update_count > 500:
             #     break
+
+        train_epoch_eer = utils.compute_eer_total(all_predictions, all_targets)
+        writer.add_scalar('train_eer', train_epoch_eer, state['step'])
 
     # VALIDATE
     val_loss, val_eer, val_accuracy = validate(args, model, criterion, val_data)